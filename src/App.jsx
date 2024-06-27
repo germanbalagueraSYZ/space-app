aec8f31bfa7d92413339d4d3d5abb55567cd7265
germanbalagueraSYZ <german.balaguera@grupo-syz.com>
2024-06-27 12:16:05 -0500

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,22 @@ import Galeria from "./components/Galeria"
 import fotos from "./fotos.json"
 import { useState } from "react"
 import ModalZoom from "./components/ModalZoom"
-
+import Pie from "./components/Pie"
 
 const FondoGradiente = styled.div`
   background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width:100%;
   min-height:100vh;
 `
-
 const AppContainer = styled.div`
   width:1280px;
   max-width:100%;
   margin: 0 auto;
 `
-
 const MainContainer = styled.main`
   display: flex;
   gap:24px;
 `
-
 const ContenidoGaleria = styled.section`
   display: flex;
   flex-direction: column;
@@ -36,14 +33,13 @@ const ContenidoGaleria = styled.section`
 const App = () => {
   const [fotosDeGaleria, setFotosDeGaleria] = useState(fotos)
   const [fotoSeleccionada, setFotoSeleccionada] = useState(null)
-  const alAlternarFavorito = (foto) => {
 
+  const alAlternarFavorito = (foto) => {
     if (foto.id === fotoSeleccionada?.id) {
       setFotoSeleccionada({
         ...fotoSeleccionada,
         favorita: !fotoSeleccionada.favorita
       })
-
     }
 
     setFotosDeGaleria(fotosDeGaleria.map(fotoDeGaleria => {
@@ -71,6 +67,7 @@ const App = () => {
         <ModalZoom foto={fotoSeleccionada}
           alCerrar={() => setFotoSeleccionada(null)}
           alAlternarFavorito={alAlternarFavorito} />
+        <Pie />
       </FondoGradiente>
     </>
   )
