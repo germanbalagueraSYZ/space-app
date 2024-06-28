5cccc4c9d02fd6816b79f647891bd124086168a0
germanbalagueraSYZ <german.balaguera@grupo-syz.com>
2024-06-28 16:11:43 -0500

    context API

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import banner from "./assets/banner.png";
 import Galeria from "./components/Galeria";
 import ModalZoom from "./components/ModalZoom";
 import Pie from "./components/Pie";
-import Cargando from "./components/Cargando";
-import { useEffect, useState } from "react";
+import GlobalContextProvider from "./context/GlobalContext"
 
 const FondoGradiente = styled.div`
   background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -28,68 +27,28 @@ const ContenidoGaleria = styled.section`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-`;
-
+`
 const App = () => {
-
-  const [consulta, setConsulta] = useState('')
-  const [fotosDeGaleria, setFotosDeGaleria] = useState([])
-  const [fotoSeleccionada, setFotoSeleccionada] = useState(null)
-
-  const alAlternarFavorito = (foto) => {
-
-    if (foto.id === fotoSeleccionada?.id) {
-      setFotoSeleccionada({
-        ...fotoSeleccionada,
-        favorita: !fotoSeleccionada.favorita
-      })
-
-    }
-
-    setFotosDeGaleria(fotosDeGaleria.map(fotoDeGaleria => {
-      return {
-        ...fotoDeGaleria,
-        favorita: fotoDeGaleria.id === foto.id ? !foto.favorita : fotoDeGaleria.favorita
-      }
-    }))
-  }
-
-
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch('http://localhost:3000/fotos');
-      const data = await res.json();
-      setFotosDeGaleria([...data]);
-    }
-
-    setTimeout(() => getData(), 5000);
-  }, [])
 
   return (
     <>
       <FondoGradiente>
         <GlobalStyles />
-        <AppContainer>
-          <Cabecera setConsulta={setConsulta} />
-          <MainContainer>
-            <BarraLateral />
-            <ContenidoGaleria>
-              <Banner texto="La galería más completa de fotos del espacio" backgroundImage={banner} />
-              {
-                fotosDeGaleria.length == 0 ?
-                  <Cargando></Cargando> :
-                  <Galeria alSeleccionarFoto={foto => setFotoSeleccionada(foto)}
-                    fotos={fotosDeGaleria}
-                    alAlternarFavorito={alAlternarFavorito}
-                    consulta={consulta} />
-              }
-            </ContenidoGaleria>
-          </MainContainer>
-        </AppContainer>
-        <ModalZoom foto={fotoSeleccionada}
-          alCerrar={() => setFotoSeleccionada(null)}
-          alAlternarFavorito={alAlternarFavorito} />
-        <Pie />
+        <GlobalContextProvider>
+          <AppContainer>
+            <Cabecera />
+            <MainContainer>
+              <BarraLateral />
+              <ContenidoGaleria>
+                <Banner texto="La galería más completa de fotos del espacio" backgroundImage={banner} />
+                <Galeria />
+              </ContenidoGaleria>
+            </MainContainer>
+          </AppContainer>
+          <ModalZoom />
+          <Pie />
+        </GlobalContextProvider>
+
       </FondoGradiente>
     </>
   )
--- src/components/Titulo/index.jsx
+++ src/components/Titulo/index.jsx
@@ -4,6 +4,6 @@ const Titulo= styled.h2`
 font-size: 32px;
 color:#7B78E5;
 text-align: ${props => props.$align ? props.$align : "left" };
-
 `
+
 export default Titulo
